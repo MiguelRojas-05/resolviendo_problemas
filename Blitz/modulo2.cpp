c8497ded9b6685d4c12a6e1680c42028a1a4f082
MiguelRojas-05 <miguelaimar052001@gmail.com>
2024-08-19 16:18:31 -0500

    resolucion de modulo2.0

--- Blitz/modulo2.cpp
+++ Blitz/modulo2.cpp
@@ -2,19 +2,31 @@
 
 using namespace std;
 
-void solve(){
-    
+void solve()
+{
+    string n;
+    int m;
+    cin >> n >> m;
+
+    int acum = 0;
+    for (char e : n)
+    {
+        acum = (acum * 10 + (e - '0')) % m;
+    }
+
+    cout << acum << endl;
 }
 
-
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-int t; cin>>t;
-while (t--)
-{
-    solve();
-}
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
 
     return 0;
 }
