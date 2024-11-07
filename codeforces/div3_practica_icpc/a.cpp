952358fb38154d570e3330dfbae5fb98e9402f8c
Aimar - ubuntu <miguelaimar052001@gmail.com>
2024-11-07 11:28:11 -0500

    Se añadió un problema del div3 que se hizo como práctica para el icpc 2024

---
+++ codeforces/div3_practica_icpc/a.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+const ll MOD = 1e9+7;
+const ll INF = 1e18;
+
+void solve() {
+    int n; cin >> n ;
+    if(n%2 == 0) cout << "Sakurako"<<'\n';
+    else cout << "Kosuke"<<'\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
