1981d38543311fd0384af234910fccd980a9db4d
Aimar - ubuntu <miguelaimar052001@gmail.com>
2025-01-18 22:50:16 -0500

    problema de prefix sum y también puede resolverse con two pointers en leetcode, contar cantidad de subarrays que sumen una cantidad goal

--- leetcode/binary_subarray_with_sum.cpp
+++ leetcode/binary_subarray_with_sum.cpp
@@ -2,21 +2,45 @@
 
 using namespace std;
 using ll = long long;
+
 class Solution {
+private:
+    int count(vector<int>& nums,int goal){
+        int left=0,right=0,sum=0,count=0;
+        while(right<nums.size()){
+            sum+=nums[right];
+            while(sum>goal && left<=right){
+                sum-=nums[left];
+                left++;
+            }
+
+            if(sum<=goal){
+                count+=right-left+1;
+            }
+            right++;
+        }
+        return count;
+    }
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        unordered_map<int,int> mp;
-        mp[0] = 1;
-        int sum = 0;
-        int r = 0;
-        for(int& num : nums){
-            sum += num;
-            if(mp.count(sum - goal))r+=mp[sum-goal];
-            mp[sum]++;
-        }
-        return r;
+        return count(nums,goal)-count(nums,goal-1);
     }
 };
+// class Solution {
+// public:
+//     int numSubarraysWithSum(vector<int>& nums, int goal) {
+//         unordered_map<int,int> mp;
+//         mp[0] = 1;
+//         int sum = 0;
+//         int r = 0;
+//         for(int& num : nums){
+//             sum += num;
+//             if(mp.count(sum - goal))r+=mp[sum-goal];
+//             mp[sum]++;
+//         }
+//         return r;
+//     }
+// };
 
 int main() {
     ios_base::sync_with_stdio(false);
